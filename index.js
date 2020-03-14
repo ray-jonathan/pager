4acfe1477b004fdf74302848ab1172f0e8299f0f
Ubuntu <ubuntu@ip-172-31-16-123.us-east-2.compute.internal>
2020-03-14 14:37:10 +0000

    switching hosting providors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended:true}))
 
-app.post('*',(req, res) => {
-    console.log(req)
+app.use('*',(req, res) => {
+    console.log(req.headers)
+    console.log(req.query)
+    console.log(req.body)
+	res.send("Roger that.")
 });
 
-app.listen(3000, ()=> {console.log("Listening on 3000")});
+app.listen(3413, ()=> {console.log("Listening on 3413")});
